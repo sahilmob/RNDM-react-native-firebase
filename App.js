ae1d870ea1fe110e529b665e3868f2a81dbcfca3
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-19 16:11:57 +0200

    create screens

--- App.js
+++ App.js
@@ -1,60 +1,18 @@
 import React, { Component } from "react";
-import { TouchableNativeFeedback, Text, View } from "react-native";
-import { Header, Left, Right, Fab, Button } from "native-base";
-import Icon from "react-native-vector-icons/FontAwesome5";
-import { ScaledSheet, scale } from "react-native-size-matters";
-export default class App extends Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<Header style={styles.header}>
-					<Left style={styles.headerLeft}>
-						<Text style={styles.headerText}>RNDM</Text>
-					</Left>
-					<Right style={styles.headerRight}>
-						<TouchableNativeFeedback>
-							<View style={styles.ellipsisBtn}>
-								<Icon name="ellipsis-v" color="white" />
-							</View>
-						</TouchableNativeFeedback>
-					</Right>
-				</Header>
-				<Fab position="bottomRight" style={styles.fab}>
-					<Icon name="envelope" />
-				</Fab>
-			</View>
-		);
-	}
-}
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-const styles = ScaledSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		backgroundColor: "#F5FCFF"
+import Home from "./src/screens/Home";
+import AddThought from "./src/screens/AddThought";
+
+const AppNavigation = createStackNavigator({
+	Home: {
+		screen: Home
 	},
-	header: {
-		width: "100%",
-		backgroundColor: "#363636"
-	},
-	headerLeft: {
-		marginLeft: "10@s"
-	},
-	headerRight: {
-		marginRight: "10@s"
-	},
-	headerText: {
-		color: "white"
-	},
-	ellipsisBtn: {
-		width: scale(20),
-		height: scale(20),
-		backgroundColor: "#1e1e1e",
-		borderRadius: scale(10),
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	fab: {
-		backgroundColor: "#F5820c"
+	AddThought: {
+		screen: AddThought
 	}
 });
+
+const AppContainer = createAppContainer(AppNavigation);
+
+export default AppContainer;
---
+++ src/screens/Home.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { TouchableNativeFeedback, Text, View } from "react-native";
+import { Header, Left, Right, Fab } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { ScaledSheet, scale } from "react-native-size-matters";
+
+export default class Home extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Header style={styles.header}>
+					<Left style={styles.headerLeft}>
+						<Text style={styles.headerText}>RNDM</Text>
+					</Left>
+					<Right style={styles.headerRight}>
+						<TouchableNativeFeedback>
+							<View style={styles.ellipsisBtn}>
+								<Icon name="ellipsis-v" color="white" />
+							</View>
+						</TouchableNativeFeedback>
+					</Right>
+				</Header>
+				<Fab
+					position="bottomRight"
+					style={styles.fab}
+					onPress={() => {
+						this.props.navigation.navigate("AddThought");
+					}}
+				>
+					<Icon name="envelope" />
+				</Fab>
+			</View>
+		);
+	}
+}
+
+const styles = ScaledSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		backgroundColor: "#F5FCFF"
+	},
+	header: {
+		width: "100%",
+		backgroundColor: "#363636"
+	},
+	headerLeft: {
+		marginLeft: "10@s"
+	},
+	headerRight: {
+		marginRight: "10@s"
+	},
+	headerText: {
+		color: "white"
+	},
+	ellipsisBtn: {
+		width: scale(20),
+		height: scale(20),
+		backgroundColor: "#1e1e1e",
+		borderRadius: scale(10),
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	fab: {
+		backgroundColor: "#F5820c"
+	}
+});
